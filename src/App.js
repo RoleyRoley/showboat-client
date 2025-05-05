2898b385ec3a1c9be8af62c22080c16b633fe0c5
RoleyRoley <Sonnyjohnjeffreys@gmail.com>
2025-05-05 19:29:23 +0100

    Initial commit - setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+
+// Root component of the application.
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-white text-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to SHOWBOAT</h1>
     </div>
   );
 }
